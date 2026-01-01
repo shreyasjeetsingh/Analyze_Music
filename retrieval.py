c3ce711498f2536c7119920ab51b291cfcfc9b82
Shreyas Singh <shreyasjeetsingh@gmail.com>
2026-01-01 21:57:44 +0530

    Minor Changes

--- retrieval.py
+++ retrieval.py
@@ -8,29 +8,39 @@ from insertion import songAnalysis
 def load_database():
     conn = sqlite3.connect("songs.db")
     cur = conn.cursor()
-    cur.execute("SELECT path, features FROM songs")
+    cur.execute("SELECT name, artist, features FROM songs")
     rows = cur.fetchall()
     conn.close()
 
-    paths = []
+    names = []
+    artists=[]
     vectors = []
-    for path, blob in rows:
-        paths.append(path)
+    for name, artist, blob in rows:
+        names.append(name)
+        artists.append(artist)
         vectors.append(pickle.loads(blob))
 
     X = np.vstack(vectors)
-    return paths, X
+    return names, artists, X
 
 
-paths, X = load_database()
+names, artists, X = load_database()
 
 mu = X.mean(axis=0)
 sigma = X.std(axis=0)
 sigma[sigma==0] = 1.0
 X_norm = (X - mu) / sigma
 
+song_input = input("Enter song name: ")
+conn = sqlite3.connect("songs.db")
+cur = conn.cursor()
+cur.execute("SELECT path, artist FROM songs WHERE name = ?", (song_input,))
+rows = cur.fetchall()
+conn.close()
 
-query_path = r"SomeSongs\06 - Love Is Lonely.flac"
+query_path = rows[0][0]
+artist_name = rows[0][1]
+
 y, sr = librosa.load(query_path)
 q = songAnalysis(y, sr)
 q_norm = (q - mu) / sigma
@@ -40,7 +50,7 @@ sims = cosine_similarity([q_norm], X_norm)[0]
 
 idx = np.argsort(sims)[::-1]
 
-print("Query:", query_path)
+print("Query:", song_input, "by", artist_name)
 print("\nMost Similar Songs:")
 for i in idx[:5]:
-    print(f"{paths[i]} - similarity = {sims[i]:.3f}")
+    print(f"{artists[i]} - {names[i]} : similarity = {sims[i]:.3f}")
